b2528e4f69225dc1bf2792cd9ee96afee51ccc00
half_drop <1097982745@qq.com>
2025-02-28 21:35:58 +0800

    Update NameTagMixin.java

--- common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
+++ common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
@@ -1,35 +1,24 @@
 package me.zziger.obsoverlay.mixin;
 
-import me.zziger.obsoverlay.OBSOverlayConfig;
-import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import me.zziger.obsoverlay.OverlayUtils;
 import net.minecraft.client.render.entity.EntityRenderer;
-import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.Text;
-import net.minecraft.util.math.Vec3d;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.state.EntityRenderState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderer.class)
-public abstract class NameTagMixin {
-
-    @Shadow
-    @Final
-    protected EntityRenderDispatcher dispatcher;
-
-    @Inject(at = @At("HEAD"), method = "renderLabelIfPresent(Lnet/minecraft/client/render/entity/state/EntityRenderState;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", cancellable = true)
-    private void onRenderLabelIfPresent(EntityRenderState state, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
-        if (OBSOverlayConfig.get().hidePlayerNameTags) {
+public class NameTagMixin<T extends Entity, S extends EntityRenderState> {
+    @Inject(at = @At("HEAD"), method = "render", cancellable = true)
+    private void doNotRenderNametags(S state, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        // Prevent name tags from rendering if the OverlayUtils condition is met
+        if (OverlayUtils.shouldHideNametags((T) state.getEntity())) {
             ci.cancel();
         }
     }
-
-    @Shadow
-    public abstract TextRenderer getTextRenderer();
 }
