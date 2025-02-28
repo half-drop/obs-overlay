014f333f440c188c3c313c415449a35fe00dcc45
half_drop <1097982745@qq.com>
2025-02-28 21:54:45 +0800

    Update NameTagMixin.java

--- common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
+++ common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
@@ -1,9 +1,9 @@
 package me.zziger.obsoverlay.mixin;
 
-import me.zziger.obsoverlay.OverlayUtils;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
-import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
@@ -14,9 +14,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderer.class)
-public class NameTagMixin<T extends Entity, S extends EntityRenderState> {
+public class NameTagMixin<T extends Entity> {
     @Inject(at = @At("HEAD"), method = "render", cancellable = true)
-    private void doNotRenderNametags(S state, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+    private void doNotRenderNametags(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
         // Prevent name tags from rendering if the OverlayUtils condition is met
             ci.cancel();
         
