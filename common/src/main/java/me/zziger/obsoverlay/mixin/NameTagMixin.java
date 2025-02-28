6f6adfc075aaf37a45a9417dbcfb9d23cbf22494
half_drop <1097982745@qq.com>
2025-03-01 00:25:36 +0800

    Update NameTagMixin.java

--- common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
+++ common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
@@ -14,26 +14,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import me.zziger.obsoverlay.OverlayRenderer;
 import me.zziger.obsoverlay.registry.AllDefaultOverlayComponents;
 
+@Mixin(DebugHud.class)
+public class DebugHudMixin {
+    @Inject(method = "render", at = @At("HEAD"))
+    private void drawStart(CallbackInfo ci) {
+        OverlayRenderer.beginDraw(AllDefaultOverlayComponents.debugMenu);
+    }
+
+    @Inject(method = "render", at = @At("RETURN"))
+    private void drawEnd(CallbackInfo ci) {
+        OverlayRenderer.endDraw(AllDefaultOverlayComponents.debugMenu);
+    }
+}
+
 @Mixin(EntityRenderer.class)
 public class NameTagMixin {
-
-    // 注入 renderLabelIfPresent 方法以便在 name tag 渲染时插入自定义渲染
     @Inject(method = "renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V", 
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/EntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V", 
                     shift = At.Shift.AFTER))
-    private void drawNameTagStart(Entity entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, float tickDelta, CallbackInfo ci) {
-        // 开始绘制 name tag 的覆盖层
+    private void drawStart(CallbackInfo ci) {
         OverlayRenderer.beginDraw(AllDefaultOverlayComponents.nameTag);
-        
-        // 清除 OpenGL 深度缓冲区，准备绘制
-        GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT);
     }
 
-    // 在 name tag 渲染结束后结束绘制
     @Inject(method = "renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V", 
             at = @At("RETURN"))
-    private void drawNameTagEnd(Entity entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, float tickDelta, CallbackInfo ci) {
-        // 结束绘制 name tag 的覆盖层
+    private void drawEnd(CallbackInfo ci) {
         OverlayRenderer.endDraw(AllDefaultOverlayComponents.nameTag);
     }
+    
 }
