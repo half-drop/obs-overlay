785d3d9a7ebff2de0bff5238d4d272d5ef57ca78
half_drop <1097982745@qq.com>
2025-02-28 23:13:52 +0800

    Update NameTagMixin.java

--- common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
+++ common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import me.zziger.obsoverlay.OverlayRenderer;
-
+import me.zziger.obsoverlay.OBSOverlayConfig;
 import java.lang.reflect.Method; // Import the Method class from java.lang.reflect
 
 @Mixin(EntityRenderer.class)
