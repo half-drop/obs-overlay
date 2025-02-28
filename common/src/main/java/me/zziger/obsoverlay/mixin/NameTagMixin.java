597476dc7e79b12bef084a715f8f0644bbb9cc60
half_drop <1097982745@qq.com>
2025-03-01 00:54:18 +0800

    Update NameTagMixin.java

--- common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
+++ common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
@@ -16,13 +16,15 @@ import me.zziger.obsoverlay.registry.AllDefaultOverlayComponents;
 
 @Mixin(EntityRenderer.class)
 public class NameTagMixin {
-    @Inject(method = "renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V", 
+    @Inject(method = "Lnet/minecraft/client/render/entity/PlayerEntityRenderer;renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V
+", 
             at = @At("HEAD"))
     private void drawStart(CallbackInfo ci) {
         OverlayRenderer.beginDraw(AllDefaultOverlayComponents.nameTag);
     }
 
-    @Inject(method = "renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V", 
+    @Inject(method = "Lnet/minecraft/client/render/entity/PlayerEntityRenderer;renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V
+", 
             at = @At("RETURN"))
     private void drawEnd(CallbackInfo ci) {
         OverlayRenderer.endDraw(AllDefaultOverlayComponents.nameTag);
