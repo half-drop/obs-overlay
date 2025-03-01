251e613b54a4aedece5b45499bd241df46b40e97
half_drop <1097982745@qq.com>
2025-03-01 17:40:14 +0800

    Update NameTagMixin.java

--- common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
+++ common/src/main/java/me/zziger/obsoverlay/mixin/NameTagMixin.java
@@ -17,19 +17,14 @@ public class NameTagMixin {
 
     @Inject(method = "renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V",at = @At("HEAD"))
     private void drawStart(Text text, CallbackInfo ci) {
-         //if (!isMinecraftUsername(text)) return;
+
          OverlayRenderer.beginDraw(AllDefaultOverlayComponents.nameTag);
     }
 
     @Inject(method = "renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V",at = @At("RETURN"))
     private void drawEnd(Text text, CallbackInfo ci) {
-         //if (!isMinecraftUsername(text)) return;
+
          OverlayRenderer.endDraw(AllDefaultOverlayComponents.nameTag);
     }
 
-    private boolean isMinecraftUsername(Text text) {
-        if (text == null) return false;
-        String name = text.getString();
-        return name.matches("^[A-Za-z0-9_]{3,16}$");
-    }
 }
